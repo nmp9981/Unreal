ae46996cb993567e607e97c328212206a9929c36
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2022-03-26 01:36:37 +0900

    Create ArenaBattle.cpp

---
+++ tutorial/chap3/ArenaBattle.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ArenaBattle.h"
+#include "Modules/ModuleManager.h"
+
+DEFINE_LOG_CATEGORY(ArenaBattle);//ArenaBattle의 로그 매크로 참조
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ArenaBattle, "ArenaBattle" );
