b792c918660da35ce67d801b7eac16e3e88a9eb8
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2022-03-26 01:36:04 +0900

    Create ArenaBattle.h

---
+++ tutorial/chap3/ArenaBattle.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+//해당 로그 매크로를 선언하고 모듈의 모든 헤더들이 이 헤더를 참조
+#pragma once
+
+#include "EngineMinimal.h"
+#include "CoreMinimal.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(ArenaBattle, Log, All);
+
+//간편하게 로그 남기기
+#define ABLOG_CALLINFO (FString(__FUNCTION__)+TEXT("(")+FString::FromInt(__LINE__)+TEXT(")") )
+//로그를 사용한 함수의 실행시점 남기기
+#define ABLOG_S(Verbosity) UE_LOG(ArenaBattle,Verbosity,TEXT("%s"),*ABLOG_CALLINFO)
+//ABLOG_S 정보에 형식 문자열로 추가 정보를 지정해 로그를 남긴다
+#define ABLOG(Verbosity,Format,...) UE_LOG(ArenaBattle,Verbosity,TEXT("%s%s"),*ABLOG_CALLINFO, *FString::Printf(Format,##__VA_ARGS__))
+
+
