a0c55fa0fd05b6b48ee06199c3dec2bfa057e521
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2022-03-29 16:59:25 +0900

    Create ABPawn.cpp

---
+++ tutorial/chap4/ABPawn.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ABPawn.h"
+
+// Sets default values
+AABPawn::AABPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AABPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AABPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+//로그 확인
+void AABPawn::PostInitializeComponents() {
+	Super::InitializeComponents();
+	ABLOG_S(Warning);
+}
+void AABPawn::PossessedBy(AController* NewController) {
+	ABLOG_S(Warning);
+	Super::PossessedBy(NewController);
+}
+
+// Called to bind functionality to input
+void AABPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
