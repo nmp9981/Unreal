45197b1012a2573832da95e06ddb16425a20481e
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2022-04-03 21:07:58 +0900

    Create ABCharacter.cpp

---
+++ tutorial/chap7/ABCharacter.cpp
@@ -0,0 +1,208 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+//새로운 캐릭터
+
+#include "ABCharacter.h"
+
+// Sets default values
+AABCharacter::AABCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SPRINGARM"));
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("CAMERA"));
+
+	//상속
+	SpringArm->SetupAttachment(GetCapsuleComponent());
+	Camera->SetupAttachment(SpringArm);
+	//초기위치
+	GetMesh()->SetRelativeLocationAndRotation(FVector(0.0f, 0.0f, -88.0f), FRotator(0.0f, -90.0f, 0.0f));
+	SpringArm->TargetArmLength = 400.0f;
+	SpringArm->SetRelativeRotation(FRotator(-15.0f, 0.0f, 0.0f));
+	//애셋지정
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> SK_CARDBOARD(TEXT("/Game/InfinityBladeWarriors/Character/CompleteCharacters/SK_CharM_Cardboard.SK_CharM_Cardboard"));
+	if (SK_CARDBOARD.Succeeded()) GetMesh()->SetSkeletalMesh(SK_CARDBOARD.Object);
+	//애니메이션 지정
+	GetMesh()->SetAnimationMode(EAnimationMode::AnimationBlueprint);
+	static ConstructorHelpers::FClassFinder<UAnimInstance> WARRIOR_ANIM(TEXT("/Game/Book/Animations/WarriorAnimBlueprint.WarriorAnimBlueprint"));
+	if (WARRIOR_ANIM.Succeeded()) GetMesh()->SetAnimInstanceClass(WARRIOR_ANIM.Class);
+
+	SetControlMode(EControlMode::DIABLO);
+
+	//속도지정
+	ArmLengthSpeed = 3.0f;
+	ArmRotationSpeed = 10.0f;
+	GetCharacterMovement()->JumpZVelocity = 800.0f;//캐릭터 점프 높이 조절
+}
+
+// Called when the game starts or when spawned
+void AABCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+/*
+//GTA방식 3인칭 조작
+void AABCharacter::SetControlMode(int32 ControlMode) {
+	if (ControlMode == 0) {//0번 모드
+		//SpringArm->TargetArmLength = 450.0f;
+		//SpringArm->SetRelativeRotation(FRotator::ZeroRotator);
+		SpringArm->bUsePawnControlRotation = true;
+		//회전여부
+		SpringArm->bInheritPitch = true;
+		SpringArm->bInheritRoll = true;
+		SpringArm->bInheritYaw = true;
+		SpringArm->bDoCollisionTest = true;
+		bUseControllerRotationYaw = false;
+		//캐릭터가 움직이는 방향으로 자동회전
+		GetCharacterMovement()->bOrientRotationToMovement = true;
+		GetCharacterMovement()->RotationRate = FRotator(0.0f, 720.0f, 0.0f);
+	}
+}
+*/
+//디아블로 방식 3인칭 구현
+void AABCharacter::SetControlMode(EControlMode NewControlMode) {
+	CurrentControlMode = NewControlMode;//컨트롤 모드 값
+
+	switch (CurrentControlMode) {
+	case EControlMode::GTA:
+		//SpringArm->TargetArmLength = 450.0f;
+		//SpringArm->SetRelativeRotation(FRotator::ZeroRotator);
+		ArmLengthTo = 450.0f;
+		SpringArm->bUsePawnControlRotation = true;
+		SpringArm->bInheritPitch = true;
+		SpringArm->bInheritRoll = true;
+		SpringArm->bInheritYaw = true;
+		SpringArm->bDoCollisionTest = true;
+		bUseControllerRotationYaw = false;
+		GetCharacterMovement()->bOrientRotationToMovement = true;
+		GetCharacterMovement()->bUseControllerDesiredRotation = false;//캐릭터의 회전 보완
+		GetCharacterMovement()->RotationRate = FRotator(0.0f, 720.0f, 0.0f);
+		break;
+	case EControlMode::DIABLO:
+		//SpringArm->TargetArmLength = 800.0f;
+		//SpringArm->SetRelativeRotation(FRotator(-45.0f, 0.0f, 0.0f));
+		ArmLengthTo = 800.0f;
+		ArmRotationTo = FRotator(-45.0f, 0.0f, 0.0f);
+		SpringArm->bUsePawnControlRotation = false;
+		SpringArm->bInheritPitch = false;
+		SpringArm->bInheritRoll = false;
+		SpringArm->bInheritYaw = false;
+		SpringArm->bDoCollisionTest = false;
+		bUseControllerRotationYaw = false;
+		GetCharacterMovement()->bOrientRotationToMovement = false;
+		GetCharacterMovement()->bUseControllerDesiredRotation = true;//캐릭터의 회전 보완
+		GetCharacterMovement()->RotationRate = FRotator(0.0f, 720.0f, 0.0f);
+		break;
+	}
+}
+
+// Called every frame
+void AABCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	SpringArm->TargetArmLength = FMath::FInterpTo(SpringArm->TargetArmLength, ArmLengthTo, DeltaTime, ArmLengthSpeed);//목표까지 서서히 변경
+	//목표지점까지 서서히 변경
+	switch (CurrentControlMode) {
+	case EControlMode::DIABLO:
+		SpringArm -> RelativeRotation = FMath::RInterpTo(SpringArm -> RelativeRotation, ArmRotationTo, DeltaTime, ArmRotationSpeed);
+		break;
+	}
+
+	switch (CurrentControlMode) {
+	case EControlMode::DIABLO:
+		if (DirectionToMove.SizeSquared() > 0.0f) {
+			GetController()->SetControlRotation(FRotationMatrix::MakeFromX(DirectionToMove).Rotator());
+			AddMovementInput(DirectionToMove);//캐릭터 움직임 설정
+		}
+		break;
+	}
+
+}
+
+// Called to bind functionality to input
+void AABCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	//액션매핑 입력설정과 연동
+	PlayerInputComponent->BindAction(TEXT("ViewChange"), EInputEvent::IE_Pressed, this, &AABCharacter::ViewChange);
+	//ACharacter클래스에는 Jump라는 멤버함수가 있다.
+	PlayerInputComponent->BindAction(TEXT("Jump"), EInputEvent::IE_Pressed, this, &ACharacter::Jump);
+	//방향키 이동
+	PlayerInputComponent->BindAxis(TEXT("UpDown"), this, &AABCharacter::UpDown);
+	PlayerInputComponent->BindAxis(TEXT("LeftRight"), this, &AABCharacter::LeftRight);
+	//마우스 좌우이동
+	PlayerInputComponent->BindAxis(TEXT("LookUp"), this, &AABCharacter::LookUp);
+	PlayerInputComponent->BindAxis(TEXT("Trun"), this, &AABCharacter::Trun);
+}
+/*
+//캐릭터 움직임 설정, 회전값으로부터 시선방향, 우측값의 벡터 가져오기
+void AABCharacter::UpDown(float NewAxisValue) {
+	AddMovementInput(FRotationMatrix(GetControlRotation()).GetUnitAxis(EAxis::X), NewAxisValue);//x축 : 시선방향
+}
+void AABCharacter::LeftRight(float NewAxisValue) {
+	AddMovementInput(FRotationMatrix(GetControlRotation()).GetUnitAxis(EAxis::Y), NewAxisValue);//y축 : 우측방향
+}
+*/
+//위아래 x축
+void AABCharacter::UpDown(float NewAxisValue) {
+	switch (CurrentControlMode) {
+	case EControlMode::GTA:
+		AddMovementInput(FRotationMatrix(FRotator(0.0f,GetControlRotation().Yaw,0.0f)).GetUnitAxis(EAxis::X), NewAxisValue);//x축 : 시선방향
+		break;
+	case EControlMode::DIABLO:
+		DirectionToMove.X = NewAxisValue;
+		break;
+	}
+}
+//좌우 y축
+void AABCharacter::LeftRight(float NewAxisValue) {
+	switch (CurrentControlMode) {
+	case EControlMode::GTA:
+		AddMovementInput(FRotationMatrix(FRotator(0.0f, GetControlRotation().Yaw, 0.0f)).GetUnitAxis(EAxis::Y), NewAxisValue);//y축 : 시선방향
+		break;
+	case EControlMode::DIABLO:
+		DirectionToMove.Y = NewAxisValue;
+		break;
+	}
+}
+
+//마우스 좌우이동
+/*
+void AABCharacter::LookUp(float NewAxisValue) {
+	AddControllerPitchInput(NewAxisValue);
+}
+*/
+void AABCharacter::LookUp(float NewAxisValue) {
+	switch (CurrentControlMode) {
+	case EControlMode::GTA:
+		AddControllerPitchInput(NewAxisValue);
+		break;
+	}
+}
+/*
+void AABCharacter::Trun(float NewAxisValue) {
+	AddControllerYawInput(NewAxisValue);
+}
+*/
+void AABCharacter::Trun(float NewAxisValue) {
+	switch (CurrentControlMode) {
+	case EControlMode::GTA:
+		AddControllerYawInput(NewAxisValue);
+		break;
+	}
+}
+
+//시점변환 액션매핑, 캐릭터 컨트롤 변경
+void AABCharacter::ViewChange() {
+	switch (CurrentControlMode) {
+	case EControlMode::GTA:
+		GetController()->SetControlRotation(GetActorRotation());//캐릭터 방향
+		SetControlMode(EControlMode::DIABLO);
+		break;
+	case EControlMode::DIABLO:
+		GetController()->SetControlRotation(SpringArm->RelativeRotation);//SpringArm회전
+		SetControlMode(EControlMode::GTA);
+		break;
+	}
+}
